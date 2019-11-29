6038bf525899ec1ba4e217120a80ab67ae72e4ab
leoserpa14 <lscordeiro14@gmail.com>
2019-11-28 19:55:13 -0700

    init

---
+++ HelloWorldApp/HelloWorldApp.java
@@ -0,0 +1,19 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+ // According to VS Code tutorial, I can simply press F5 to run my java program (or debug it)
+
+
+
+ class HelloWorldApp {
+     public static void main(String[] args) {
+         System.out.println("Hello World!");    // display the string         
+     }
+ }
+
+
+/* First step to compile: in the cmd promp type 'javac HelloWorldApp.java'
+   After obtaining the 'HelloWorldApp.class' file, enter 'java -cp . HelloWorldApp'
+*/
